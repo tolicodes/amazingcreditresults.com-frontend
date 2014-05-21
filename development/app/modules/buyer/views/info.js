c765a039b5c08083620b6d103f4a8b1d7dd4d381
Sanjay Kumar <20sanjaykumar@gmail.com>
2014-05-21 23:19:33 +0530

    bux fix

--- development/app/modules/buyer/views/info.js
+++ development/app/modules/buyer/views/info.js
@@ -24,9 +24,9 @@ define([
 		
 		// schema to generate form
 		schema: {
-	        'FirstName': {type: 'Text', title: "First Name"},
-	        'LastName':  {type: 'Text', title: "Last Name"},
-	        'Email':     { validators: ['required', 'email'] },
+	        'givenName': {type: 'Text', title: "First Name"},
+	        'familyName':  {type: 'Text', title: "Last Name"},
+	        'email':     { validators: ['required', 'email'] },
 	        'Address':   {type: 'TextArea', title: "Address"},
 	        'City':      {type: 'Text', title: "City"},
 	        'State':     { type: 'Select', 
@@ -46,10 +46,14 @@ define([
 		},
 		
 		initializeBefore : function(options) {
-			if(options && options[0] && options[0].userDetail) {
+			options = [];
+			options[0] = {};
+			options[0].userDetail = {"id":"537cbbbf741911cf03c928d3","huntKey":"cfed2e155c99b16d37a2cad951421ea344852adf1a5de90f9b7d9bb34fed9002e7492eb60a72016422792efcb40349c40e65453f860a85fd4dc33175cca80bf3","email":"20sanjaykumar@gmail.com","name":{"familyName":"Kumar","givenName":"Sanjay"},"gravatar":"https://secure.gravatar.com/avatar/4ad1325118e913f712c4b361d18b83db.jpg?s=80&d=wavatar&r=g","gravatar30":"https://secure.gravatar.com/avatar/4ad1325118e913f712c4b361d18b83db.jpg?s=30&d=wavatar&r=g","gravatar50":"https://secure.gravatar.com/avatar/4ad1325118e913f712c4b361d18b83db.jpg?s=50&d=wavatar&r=g","gravatar80":"https://secure.gravatar.com/avatar/4ad1325118e913f712c4b361d18b83db.jpg?s=80&d=wavatar&r=g","gravatar100":"https://secure.gravatar.com/avatar/4ad1325118e913f712c4b361d18b83db.jpg?s=100&d=wavatar&r=g","root":false,"accountVerified":true,"profile":{"needQuestionnaire":false}};
+			//if(options && options[0] && options[0].userDetail) {
 				this.model = new model();
 				this.model.set(options[0].userDetail);
-			}
+			//	this.render();
+			//}
 		}
 	});
 });
